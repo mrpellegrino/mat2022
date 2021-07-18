e69817adb1b53d02e9b234bdfc51e11f425efed6
Daniel Oliveira <oficial.danieloliveira@gmail.com>
2021-07-18 17:34:21 -0300

    fix(api): change url for beta

--- src/Services/api.ts
+++ src/Services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const backendURL =
   process.env.NODE_ENV === 'production'
-    ? 'https://enrollments-api.colegiosantiago.com.br/'
+    ? 'https://enrollments-api-beta.colegiosantiago.com.br/'
     : 'http://localhost:3333/';
 
 const api = axios.create({
